7d89e8fcfe53f079e966b4cba075d3bf8699c88e
kiiski10 <pekka@pekka.pl>
2023-01-07 23:28:08 +0200

    show commands for sensors

--- brain/sensor_api/serializers.py
+++ brain/sensor_api/serializers.py
@@ -9,35 +9,35 @@ from .models import (
 class TempSensorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = TempSensor
-        fields = ['pin_number', 'description', 'unit', 'label']
+        fields = ['pin_number', 'description', 'unit', 'label', 'command']
 
 
 class FlowSensorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = FlowSensor
-        fields = ['pin_number', 'description', 'unit', 'label']
+        fields = ['pin_number', 'description', 'unit', 'label', 'command']
 
 
 class RotationSensorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = RotationSensor
-        fields = ['pin_number', 'description', 'unit', 'label']
+        fields = ['pin_number', 'description', 'unit', 'label', 'command']
 
 
 class SpeedSensorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = SpeedSensor
-        fields = ['pin_number', 'description', 'unit', 'label']
+        fields = ['pin_number', 'description', 'unit', 'label', 'command']
 
 
 class HumiditySensorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = HumiditySensor
-        fields = ['pin_number', 'description', 'unit', 'label']
+        fields = ['pin_number', 'description', 'unit', 'label', 'command']
 
 
 class PressureSensorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = PressureSensor
-        fields = ['pin_number', 'description', 'unit', 'label']
+        fields = ['pin_number', 'description', 'unit', 'label', 'command']
 
