57a6ba9253c34b33254db352cf23800cfc97bef2
kiiski10 <pekka@pekka.pl>
2023-01-07 23:09:33 +0200

    register new models to admin

--- brain/sensor_api/admin.py
+++ brain/sensor_api/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from sensor_api.models import TempSensor
+from sensor_api.models import (
+    TempSensor, FlowSensor, RotationSensor,
+    SpeedSensor, HumiditySensor, PressureSensor
+)
 
 admin.site.register(TempSensor)
+admin.site.register(FlowSensor)
+admin.site.register(RotationSensor)
+admin.site.register(SpeedSensor)
+admin.site.register(HumiditySensor)
+admin.site.register(PressureSensor)
