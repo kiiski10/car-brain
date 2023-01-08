99153fc73c936401d78e4a5dfc3fd37c9554eace
kiiski10 <pekka@pekka.pl>
2023-01-08 04:16:08 +0200

    reorder serialized fields

--- brain/sensor_api/serializers.py
+++ brain/sensor_api/serializers.py
@@ -6,5 +6,12 @@ from .models import Sensor
 class SensorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Sensor
-        fields = ['pk', 'command_args', 'description', 'unit', 'label', 'value']
+        fields = [
+            'pk', 
+            'command_args', 
+            'label', 
+            'value',
+            'unit', 
+            'description', 
+        ]
 
